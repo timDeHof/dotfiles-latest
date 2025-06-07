fc25a5043f664ff5cce207611569b08778636bde
timDeHof <ttdehof@gmail.com>
2025-06-07 19:40:30 -0400

    Add CPU usage display and network status indicators to SketchyBar

--- sketchybar/felixkratz-linkarzu/items/cpu.sh
+++ sketchybar/felixkratz-linkarzu/items/cpu.sh
@@ -11,7 +11,8 @@ cpu_top=(
 
 cpu_percent=(
   label.font="$FONT:Heavy:12"
-  label=CPU
+  label=CPU%
+  label.color=$WHITE
   y_offset=-4
   padding_right=15
   width=55
@@ -51,3 +52,32 @@ sketchybar --add item cpu.top right              \
                                                  \
            --add graph cpu.user right 75         \
            --set cpu.user "${cpu_user[@]}"
+
+network_down=(
+	y_offset=-7
+	label.font="$FONT:Heavy:10"
+	label.color="$TEXT"
+	icon="$NETWORK_DOWN"
+	icon.font="$NERD_FONT:Bold:16.0"
+	icon.color="$GREEN"
+	icon.highlight_color="$BLUE"
+	update_freq=1
+)
+
+network_up=(
+	background.padding_right=-70
+	y_offset=7
+	label.font="$FONT:Heavy:10"
+	label.color="$TEXT"
+	icon="$NETWORK_UP"
+	icon.font="$NERD_FONT:Bold:16.0"
+	icon.color="$GREEN"
+	icon.highlight_color="$BLUE"
+	update_freq=1
+	script="$PLUGIN_DIR/stats/scripts/network.sh"
+)
+
+sketchybar 	--add item network.down right 						\
+						--set network.down "${network_down[@]}" 	\
+						--add item network.up right 							\
+						--set network.up "${network_up[@]}"
